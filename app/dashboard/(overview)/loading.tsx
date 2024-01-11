ec30f1ed6c2e7f5065a030c8fd20a37bd1114c6b
plumbnathan@gmail.com <plumbnathan@gmail.com>
2024-01-11 14:28:38 -0600

    pre Chapter 14 commit

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+/* eslint-disable react/display-name */
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
